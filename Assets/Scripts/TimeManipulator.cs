01a943129f333b280f3db630db775f6c2318e4ad
Payo502 <patriciogutierrez236@gmail.com>
2023-12-07 16:01:08 +0100

    Bow and most sounds, walking i am still working

--- Assets/Scripts/TimeManipulator.cs
+++ Assets/Scripts/TimeManipulator.cs
@@ -197,6 +197,8 @@ public class TimeManipulator : MonoBehaviour
 
                 lastPickedUpObject.ResetTimeManipulation();
                 lastPickedUpObject.OnPickedUp();
+
+                GameAudioManager.PlaySound(GameAudioManager.Sound.QuickGrab);
             }
         }
     }
